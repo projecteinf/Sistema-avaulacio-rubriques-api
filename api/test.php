80541aa464ee1346ef35a2b6648f3a0d3fe99a22
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-07-01 08:48:12 -0400

    Instal·lació MongoDB (PHPLIB)

--- api/test.php
+++ api/test.php
@@ -1,5 +1,13 @@
 <?php
+require 'vendor/autoload.php'; // include Composer's autoloader
+
 $client = new MongoDB\Client(
     'mongodb+srv://root:a@localhost/test?retryWrites=true&w=majority'
 );
-$db = $client->test;
+
+$collection = $client->demo->beers;
+
+$result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+
+echo "Inserted with Object ID '{$result->getInsertedId()}'";
+?>
