378bff3f7f7a151ee81d4ef5f98e9da6633b6b4c
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-07-01 07:13:03 -0400

    Eliminar volums locals Docker

--- docker/server/mongo/mongo-init.js
+++ docker/server/mongo/mongo-init.js
@@ -1,9 +1,15 @@
 db.auth('root', 'a')
 
-db = db.getSiblingDB('test-database')
+db = db.getSiblingDB('rubrica')
 
 db.createUser({
-  user: "theabnermatheus",
-  pwd: "teste",
+  user: "professor",
+  pwd: "p",
   roles: [{role: "readWrite", db: "rubrica"}]
+});
+
+db.createUser({
+    user: "alumne",
+    pwd: "a",
+    roles: [{role: "read", db: "rubrica"}]
 });
