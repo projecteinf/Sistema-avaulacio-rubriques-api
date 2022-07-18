6d851bfbd825ed6fcbb1182a814cd04024e319c0
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-07-18 06:58:50 -0400

    Instal·lació driver MONGO amb apt-get

--- api/app/model/PersistenceLayer/test.php
+++ api/app/model/PersistenceLayer/test.php
@@ -3,11 +3,24 @@
     // require_once __DIR__."/implementacio/MongoDb.php";
     
 
-    $con = new MongoDB\Driver\Manager("mongodb://root:a@127.0.0.1:27017");
+    $con = new MongoDB\Driver\Manager("mongodb://root:a@127.0.0.1:27017/rubrica");
     if($con) {
-        $list = new MongoDB\Driver\Command(["listCollections"=>1]);
-        $res = $con->executeCommand("admin",$list);
-        var_dump($list);
+        try {
+            $bulk = new MongodDB\Driver\BulkWrite;
+            $bulk->insertOne(['nom' => 'TestUsuari']);
+            $con->executeBulkWrite('rutina.usuaris',$bulk);
+        } catch (Trowable $e) {
+            echo "Error {$e->getMessage()}";
+        }
+
+
+        //$filter = [];$options = [];
+        //$query = new MongoDB\Driver\Query($filter,$options);
+        //$rows = $con->executeQuery('rubrica.usuaris',$query);
+        //$rows = $con->connexio->executeQuery('rubrica.usuaris',$query);
+        //$list = new MongoDB\Driver\Command(["listCollections"=>1]);
+        //$res = $con->executeCommand("admin",$list);
+        var_dump($con);
     }
     else echo "ERROR";
     // $filter = ['user'=>'acalvo'];
