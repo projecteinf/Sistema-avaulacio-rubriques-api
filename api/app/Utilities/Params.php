e535fdb0d3e6b9786839120990967d26d1366bc5
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-07-25 08:33:59 +0000

    Pròrroga token

--- api/app/Utilities/Params.php
+++ api/app/Utilities/Params.php
@@ -1,7 +1,9 @@
 <?php
+// https://datatracker.ietf.org/doc/html/rfc7519#section-4
     class Params {
         const SECRET_KEY = "YOUR_SECRET_KEY"; // No hauria d'estar aquí!
         const ISSUER_CLAIM = "DOCKER_PHP_1";
         const AUDIENCE_CLAIM = "USER"; 
-        const SEGONS_DURADA_TOKEN = 300;
+        const SEGONS_DURADA_TOKEN = 300; // Segons
+        const PRORROGA_TOKEN = 50; // % . Si TEMPS_TRANSCORREGUT>=SEGONS_DURADA_TOKEN*PRORROGA_TOKEN/100 => ProrrogarToken()
 }
