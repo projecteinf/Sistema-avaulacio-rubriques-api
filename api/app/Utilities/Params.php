3403376ec9c350750650b2c0fb5dcbd8a4e730fc
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-07-25 08:27:56 +0000

    Regenerar Token (sempre)

--- api/app/Utilities/Params.php
+++ api/app/Utilities/Params.php
@@ -2,6 +2,6 @@
     class Params {
         const SECRET_KEY = "YOUR_SECRET_KEY"; // No hauria d'estar aquí!
         const ISSUER_CLAIM = "DOCKER_PHP_1";
-        const AUDIENCE_CLAIM = "THE_AUDIENCE"; 
+        const AUDIENCE_CLAIM = "USER"; 
         const SEGONS_DURADA_TOKEN = 300;
 }
