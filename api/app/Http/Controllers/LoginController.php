5181fa803b036bc22bba4cd979a19af97a459ea7
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-10-07 09:14:51 +0000

    Incloure cursos en el token

--- api/app/Http/Controllers/LoginController.php
+++ api/app/Http/Controllers/LoginController.php
@@ -43,7 +43,8 @@ class LoginController extends Controller
             "data" => array(
                 "id" => $data->id,
                 "name" => $data->name,
-                "rol" => $data->rol
+                "rol" => $data->rol,
+                "cursos" => $data->cursos
             ));
     }
 
