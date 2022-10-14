3bb598fea44df9d651d49a93e24fbdc730dfbd3d
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-10-14 10:47:28 +0000

    Eliminar password llistat alumnes

--- api/app/model/Entities/implementacio/Login.php
+++ api/app/model/Entities/implementacio/Login.php
@@ -34,7 +34,7 @@
 
         public function getStudents($con) { 
             $filter = ['rol' => "student"];
-            $options = [];
+            $options = [ 'projection' => ['password' => 0 , '_id' => 0]];
             $query = new \MongoDB\Driver\Query($filter,$options);
             $rows = $con->executeQuery('rubrica.usuaris',$query);
 
